322e09f7613366dc364654cd30f0601c2983e9d7
wminstone <wminstone@gmail.com>
2026-01-15 14:49:11 +0900

    초기 커밋.

---
+++ Tetris/TetrisView.cpp
@@ -0,0 +1,105 @@
+﻿
+// TetrisView.cpp: CTetrisView 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS는 미리 보기, 축소판 그림 및 검색 필터 처리기를 구현하는 ATL 프로젝트에서 정의할 수 있으며
+// 해당 프로젝트와 문서 코드를 공유하도록 해 줍니다.
+#ifndef SHARED_HANDLERS
+#include "Tetris.h"
+#endif
+
+#include "TetrisDoc.h"
+#include "TetrisView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CTetrisView
+
+IMPLEMENT_DYNCREATE(CTetrisView, CView)
+
+BEGIN_MESSAGE_MAP(CTetrisView, CView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CTetrisView 생성/소멸
+
+CTetrisView::CTetrisView() noexcept
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CTetrisView::~CTetrisView()
+{
+}
+
+BOOL CTetrisView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CTetrisView 그리기
+
+void CTetrisView::OnDraw(CDC* /*pDC*/)
+{
+	CTetrisDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+
+// CTetrisView 인쇄
+
+BOOL CTetrisView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CTetrisView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CTetrisView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+
+// CTetrisView 진단
+
+#ifdef _DEBUG
+void CTetrisView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CTetrisView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CTetrisDoc* CTetrisView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CTetrisDoc)));
+	return (CTetrisDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CTetrisView 메시지 처리기
